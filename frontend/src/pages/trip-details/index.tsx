86e73541e29f388373201d95abe3d4762179cd5a
Carlosjw <carlos.ti.infor@gmail.com>
2024-07-13 21:50:17 -0300

    updateProject

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,3 +1,7 @@
 export function TripDetailsPage(){
-    return <h1>The trips details page</h1>
+    return (
+        <div className="max-w-6xl px-6 py-10">
+            <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape"></div>
+        </div>
+    )
 }
